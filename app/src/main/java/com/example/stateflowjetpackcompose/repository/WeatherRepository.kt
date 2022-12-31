2612a5a884cf8fde501d5f2cfa1915f79274189d
klaus19 <tejaskhartude@gmail.com>
2022-12-31 19:16:05 +0530

    Updated Repository

--- app/src/main/java/com/example/stateflowjetpackcompose/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/stateflowjetpackcompose/repository/WeatherRepository.kt
@@ -1,4 +1,11 @@
 package com.example.stateflowjetpackcompose.repository
 
-class WeatherRepository {
+import com.example.stateflowjetpackcompose.model.NetworkingService
+import com.example.stateflowjetpackcompose.model.WeatherApiResponse
+import javax.inject.Inject
+
+class WeatherRepository @Inject constructor(private val networkingService:NetworkingService) {
+
+    suspend fun fetchWeather(long: String, lat: String): WeatherApiResponse =
+        networkingService.fetchWeather(lon = long, lat = lat)
 }
